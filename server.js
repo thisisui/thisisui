920e7be1f67c34353d94b5925b7b0658c073ce94
Igor Napierala <igor.napierala@cognifide.com>
2018-05-28 15:58:33 +0200

    https redirection

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
-var http = require('http');
 var enforce = require('express-sslify');
-
+var path = require('path');
 var app = express();
 
 // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
 // a load balancer (e.g. Heroku). See further comments below
 app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+// viewed at http://localhost:8080
+app.get('/index.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.listen(8081);
